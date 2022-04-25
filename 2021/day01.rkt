9c7301033a7c34a6da4da5621a59571a446a0ec7
amacy <aaronmacy@gmail.com>
2022-04-25 15:18:56 -0400

    01 01: solved

--- 2021/day01.rkt
+++ 2021/day01.rkt
@@ -8,3 +8,8 @@
     [else (+ 0 (part1 (cdr queue) (cons (car queue) processed)))]))
 
 (part1 '(199 200 208 210 200 207 240 269 260 263) '())
+
+(define file-contents
+  (port->string (open-input-file "2021/input/day01.txt") #:close? #t))
+
+(part1 (map (lambda (n) (string->number n)) (string-split file-contents "\n")) '())
