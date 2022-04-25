061d0ce3eec50cc62268381df67f08d86efcd9ac
amacy <aaronmacy@gmail.com>
2022-04-25 15:20:28 -0400

    01 01: cleanup

--- 2021/day01.rkt
+++ 2021/day01.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+(define sample-input '(199 200 208 210 200 207 240 269 260 263))
+(define input 
+  (port->string (open-input-file "2021/input/day01.txt") #:close? #t))
+
 (define (part1 queue processed)
   (cond 
     [(empty? queue) 0]
@@ -7,9 +11,5 @@
     [(> (car queue) (car processed)) (+ 1 (part1 (cdr queue) (cons (car queue) processed)))]
     [else (+ 0 (part1 (cdr queue) (cons (car queue) processed)))]))
 
-(part1 '(199 200 208 210 200 207 240 269 260 263) '())
-
-(define file-contents
-  (port->string (open-input-file "2021/input/day01.txt") #:close? #t))
-
-(part1 (map (lambda (n) (string->number n)) (string-split file-contents "\n")) '())
+(part1 sample-input '())
+(part1 (map (lambda (n) (string->number n)) (string-split input "\n")) '())
