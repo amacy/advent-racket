a7b9bc97b6c97ccb46e8a86004d9138a83d8278e
amacy <aaronmacy@gmail.com>
2022-04-25 14:56:26 -0400

    day 01: works for sample input

---
+++ 2021/day01.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (part1 queue processed)
+  (cond 
+    [(empty? queue) 0]
+    [(empty? processed) (part1 (cdr queue) (list (car queue)))]
+    [(> (car queue) (car processed)) (+ 1 (part1 (cdr queue) (cons (car queue) processed)))]
+    [else (+ 0 (part1 (cdr queue) (cons (car queue) processed)))]))
+
+(part1 '(199 200 208 210 200 207 240 269 260 263) '())
