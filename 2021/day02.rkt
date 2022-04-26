7ca3c0b7ea34e04ad04d4d28517bb6abd81446a9
amacy <aaronmacy@gmail.com>
2022-04-26 15:50:04 -0400

    02 01: solved

--- 2021/day02.rkt
+++ 2021/day02.rkt
@@ -6,38 +6,41 @@
                        "up 3"
                        "down 8"
                        "forward 2"))
+;; this function can be abstracted to a shared file
+(define input
+  (port->string (open-input-file "2021/input/day02.txt") #:close? #t))
 
 (define (line-to-pair line)
   (let* ([split (string-split line " ")]
          [int (string->number (car (cdr split)))])
     (cons (car split) int)))
 
+(define (parse-input input)
+  (map (lambda (l) (line-to-pair l)) input))
+
 (define (part1 commands coords)
-  (let* ([command (car commands)]
-         [direction (car command)]
-         [value (cdr command)]
-         [horizontal (car coords)]
+  (let ([horizontal (car coords)]
          [depth (cdr coords)])
     (cond
       [(empty? commands)
        coords]
-      [(equal? direction "forward")
+      [(equal? (car (car commands)) "forward")
        (part1 
          (cdr commands)
-         (cons (+ horizontal value) depth))]
-      [(equal? direction "down") 
+         (cons (+ horizontal (cdr (car commands))) depth))]
+      [(equal? (car (car commands)) "down") 
        (part1 
          (cdr commands)
-         (cons horizontal (- depth value)))]
-      [(equal? direction "up") 
+         (cons horizontal (+ depth (cdr (car commands)))))]
+      [(equal? (car (car commands)) "up") 
        (part1 
          (cdr commands)
-         (cons horizontal (+ depth value)))]
+         (cons horizontal (- depth (cdr (car commands)))))]
       [else (raise "Shouldn't get here")])))
 
-(define parsed-input 
-  (map 
-    (lambda (l) (line-to-pair l))
-    sample-input))
+(define sample-results (part1 (parse-input sample-input) '(0 . 0)))
+(* (car sample-results) (cdr sample-results))
 
-(part1 parsed-input '(0 . 0))
+(define results (part1 (parse-input (string-split input "\n")) '(0 . 0)))
+(* (car results) (cdr results))
+
